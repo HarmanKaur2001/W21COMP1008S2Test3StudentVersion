4bb4f815b4cb53d9d2dbad71779e82d670cb4b7f
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-04-22 12:28:20 -0400

    Initial commit

---
+++ src/models/SportStore.java
@@ -0,0 +1,37 @@
+package models;
+/*
+ * name:  Harman Kaur
+ * Student Number = 200456178
+ */
+import java.util.ArrayList;
+
+public class SportStore {
+    private String address;
+    private ArrayList<InventoryItem> inventory;
+
+    public SportStore(String address, ArrayList<InventoryItem> inventory) {
+        setAddress(address);
+        this.inventory = inventory;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+    }
+
+    public ArrayList<InventoryItem> getInventory() {
+        return inventory;
+    }
+
+    public void sellItem(InventoryItem item, int quantitySold)
+    {
+        item.setQuantitySold(quantitySold);
+    }
+
+    public double calculateProfit()
+    {
+        return -1;
+    }
+}
